cbfce5f2b0d4870c10e5ba0646f93cf062d10be9
Jihyeong Lee <ljihyeon@amazon.com>
2022-12-13 15:17:39 -0500

    a

--- src/sagemaker/debugger/interactive_apps.py
+++ src/sagemaker/debugger/interactive_apps.py
@@ -1,10 +1,10 @@
-from sagemaker.session import Session
+from sagemaker.session import Session, NOTEBOOK_METADATA_FILE
 
 
 class TensorBoardApp(object):
     """TensorBoardApp is a class for deploying a TensorBoard app."""
 
-    def __init__(self, domain_id, user_profile_name, sagemaker_session=None):
+    def __init__(self, domain_id=None, user_profile_name=None, sagemaker_session=None):
         """Initialize a TensorBoardApp.
 
         Args:
@@ -19,12 +19,41 @@ class TensorBoardApp(object):
         self.user_profile_name = user_profile_name
         self.sagemaker_session = sagemaker_session or Session()
 
-    def get_app_url(self, training_job_name=None):
+        if not domain_id or not user_profile_name:
+            self._infer_domain_and_user()
+
+    def _infer_domain_and_user(self):
+        if not os.path.exists(NOTEBOOK_METADATA_FILE):
+            return
+        
+        with open(NOTEBOOK_METADATA_FILE, "rb") as f:
+            metadata = json.loads(f.read())
+            instance_name = metadata["ResourceName"]
+            domain_id = metadata.get("DomainId")
+            user_profile_name = metadata.get("UserProfileName")
+            if domain_id and user_profile_name:
+                self.domain_id = domain_id
+                self.user_profile_name = user_profile_name
+
+
+    def get_signed_app_url(self, training_job_name=None):
+        """Gets a signed URL to the TensorBoard app.
+
+        Args:
+            training_job_name (str): The name of the training job. If not
+                specified, the training job name is read from the training job
+                input configuration.
+
+        Returns:
+            str: A signed URL to the TensorBoard app.
+        """
         response = self.sagemaker_session.sagemaker_client.create_presigned_domain_url(
             DomainId=self.domain_id,
             UserProfileName=self.user_profile_name
         )
-        app_url = response['AuthorizedUrl']response["AuthorizedUrl"]
+        app_url = response['AuthorizedUrl']
         app_url += "&redirect=tensorboard"
+        if training_job_name:
+            app_url += "/add_folder_or_job/{}".format(training_job_name)
 
         return app_url
